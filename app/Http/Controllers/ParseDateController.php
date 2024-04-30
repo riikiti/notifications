787758a4557c7c56c91fb91a4f7e36a7aba51c64
Ruslan <rik.chanel@gmail.com>
2024-04-30 14:14:58 +0300

    kondinskiy image generate start

--- app/Http/Controllers/ParseDateController.php
+++ app/Http/Controllers/ParseDateController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers;
 
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Symfony\Component\DomCrawler\Crawler;
 
 class ParseDateController extends Controller
 {
+
+    private string $URL ;
+    private array $AUTH_HEADERS;
+
+
+    public function __construct()
+    {
+        $this->URL = env('FUSIONBRAIN_URL');
+        $this->AUTH_HEADERS = [
+            'X-Key' => 'Key ' . env('FUSIONBRAIN_API'),
+            'X-Secret' => 'Secret ' . env('FUSIONBRAIN_SECRET'),
+            'Content-Type' => 'application/json',
+        ];
+    }
     public function __invoke()
     {
         $response = Http::withHeaders([
@@ -27,7 +42,27 @@ class ParseDateController extends Controller
         $pattern = '/(\d+)\s*(года|лет|год)?$/';
         $replacement = '';
         $new_str = preg_replace($pattern, $replacement, $events[0]);
-        dd($new_str);
+        // dd($holidays);
 
+
+        $client = new Client();
+        $response = $client->request('GET', $this->URL . 'key/api/v1/models', [
+            'headers' => $this->AUTH_HEADERS,
+            'json' => [
+                "type" => "GENERATE",
+                "style" => "string",
+                "width" => 1024,
+                "height" => 1024,
+                "num_images" => 1,
+                //"negativePromptUnclip" => "яркие цвета, кислотность, высокая контрастность",
+                "generateParams" => [
+                    "query" => "Пушистый кот в очках",
+                ],
+            ],
+        ]);
+
+        $data = json_decode($response->getBody(), true);
+        dd($data);
+        return $data[0]['id'];
     }
 }
