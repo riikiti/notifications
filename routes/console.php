dcdf1f3740210e8e992f7f2a1dc6d8ee9df662b2
Ruslan <rik.chanel@gmail.com>
2024-05-04 21:24:05 +0300

    fix

--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('app:send-holiday')->everyMinute();
+Schedule::command('app:send-holiday')->everyFiveMinutes();
 Schedule::command('app:clear-storage-command')->dailyAt('6:00');
 Schedule::command('app:parse-all-holidays')->dailyAt('6:00');
 Schedule::command('app:send-names-command')->dailyAt('16:00');
