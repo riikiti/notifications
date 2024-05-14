446fdba7c2764e867275f23cf50120f880b54453
Ruslan <rik.chanel@gmail.com>
2024-05-14 23:57:09 +0300

    cut the holidays number

--- app/Http/Actions/ParseDate/ParseDateHolidays.php
+++ app/Http/Actions/ParseDate/ParseDateHolidays.php
@@ -12,7 +12,8 @@ class ParseDateHolidays extends ParseData
     public function parse()
     {
         $holidays = $this->crewlerParse('h4');
-        $date = Carbon::today()->hour(8);
+        $holidays = array_slice($holidays, 0, 10);
+        $date = Carbon::today()->hour(9);
         foreach ($holidays as $holiday) {
             Holidays::create(['name' => $holiday, 'publish_in' => $date]);
             $date->addMinutes(30);
