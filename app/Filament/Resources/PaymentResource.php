4231c06e9d73e3f53554c37ff1277a6236a1c055
Ruslan <rik.chanel@gmail.com>
2024-05-31 18:53:38 +0300

    admin payment

---
+++ app/Filament/Resources/PaymentResource.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PaymentResource\Pages;
+use App\Filament\Resources\PaymentResource\RelationManagers;
+use App\Models\Payment;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PaymentResource extends Resource
+{
+    protected static ?string $model = Payment::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-currency-dollar';
+    protected static ?string $navigationLabel = 'Донаты';
+
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPayments::route('/'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/PaymentResource/Pages/ListPayments.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Resources\PaymentResource\Pages;
+
+use App\Filament\Resources\PaymentResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListPayments extends ListRecords
+{
+    protected static string $resource = PaymentResource::class;
+    protected static ?string $title = 'Донаты';
+
+}
